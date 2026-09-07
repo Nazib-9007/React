0dad3567042b1a09090030af4a1a3f3adceabec1
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-09-07 20:23:28 +0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import Card from './components/card';
-//import  TestReact  from './testReact'
+import Card from './components/card.jsx'
 
 function App() {
-  return <Card firstName="Nazib, " lastName="Ul Alam " age={25} />
+  return <Card firstName="Nazib" lastName="Ul Alam" age={25}>
+    {/* //children get from Card component... */}
+    <h1>Welcome To the React world!</h1>
+  </Card>
 }
-
 export default App
