8afc5a2e6bcf9e275524ea288c4b86254f97cba4
S M Nazib Ul Alam <nazib9007.ice.bup@gmail.com>
2026-09-19 20:58:49 +0600

    Update conditional.jsx

--- components/conditional.jsx
+++ components/conditional.jsx
@@ -1,15 +1,26 @@
 export default function Car(){
-  
   let marks = 68;
-
   return (
   <>
-  {
+  { {/* Ternary if-else condition in Jsx*/}
     marks>80 ?
     <h1>Brilliant Result!</h1>
     :
     <h1>Average Result!</h1>
   }
+  <div>
+    {/* Imidiately invoke function use for if-else condition in Jsx*/}
+    {(()=>
+        {
+          if(marks>80)
+          {
+            return <h1>Brilliant Result</h1>
+          } else
+          {
+           return <h1>Average Result</h1>
+          }
+    })()}
+  </div>
   </>
   )
 }
